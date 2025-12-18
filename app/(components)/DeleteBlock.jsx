d504784abecf5c3966fed10867ea357200c56123
Andrew Stevenson <37091999+Drewshka@users.noreply.github.com>
2025-12-18 16:36:40 -0500

    setting up mongoDB connection

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,5 +1,15 @@
+const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome");
+import { fa0, faX } from "@fortawesome/free-solid-svg-icons";
+
 const DeleteBlock = () => {
-  return <div>DeleteBlock</div>;
+  return (
+    <div className="flex items-center gap-2">
+      <FontAwesomeIcon
+        icon={faX}
+        className="text-red-400 hover:cursor-pointer hover:text-red-200"
+      />
+    </div>
+  );
 };
 
 export default DeleteBlock;
--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,14 @@
+// import TicketCard from "@/app/(components)/TicketCard";
+
 const TicketPage = async ({ params }) => {
   const { id } = await params;
 
-  return <div>TicketPage {id}</div>;
+  return (
+    <div>
+      TicketPage {id}
+      {/* <TicketCard /> */}
+    </div>
+  );
 };
 
 export default TicketPage;
