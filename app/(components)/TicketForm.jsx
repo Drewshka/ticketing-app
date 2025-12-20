32a511f42d6a1cc96d4c0b51023fca6a584e8e1e
Andrew Stevenson <37091999+Drewshka@users.noreply.github.com>
2025-12-19 20:04:16 -0500

    updating ticket form

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -24,9 +24,16 @@ const TicketForm = () => {
     }));
   };
 
+  const handleSubmit = () => {
+    console.log("Submitted");
+  };
+
   return (
     <div className="flex justify-center">
-      <form>
+      <form
+        className="flex flex-col gap-3 w-full max-w-md"
+        method="post"
+        onSubmit={handleSubmit}>
         <h3>Create Your Ticket</h3>
         <label htmlFor="title">Title</label>
         <input
@@ -38,6 +45,85 @@ const TicketForm = () => {
           value={formData.title}
           className="bg-white text-black border border-gray-200 rounded p-1 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2"
         />
+        <label htmlFor="title">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          type="text"
+          onChange={handleChange}
+          required
+          value={formData.description}
+          className="bg-white text-black border border-gray-200 rounded p-1 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2"
+          rows="5"
+        />
+
+        <label>Category</label>
+        <select
+          className="bg-white text-black border border-gray-200 rounded p-1 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2"
+          name="category"
+          value={formData.category}
+          onChange={handleChange}>
+          <option value={"Hardware Problem"}>Hardware Problem</option>
+          <option value={"Software Problem"}>Software Problem</option>
+          <option value={"Project"}>Project</option>
+        </select>
+
+        <label>Priority</label>
+        <input
+          id="priority-1"
+          name="priority"
+          type="radio"
+          onChange={handleChange}
+          required
+          value={1}
+          checked={formData.priority == "1"}
+          className="bg-white text-black border border-gray-200 rounded p-1 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2"
+        />
+        <label>1</label>
+        <input
+          id="priority-2"
+          name="priority"
+          type="radio"
+          onChange={handleChange}
+          required
+          value={2}
+          checked={formData.priority == "2"}
+          className="bg-white text-black border border-gray-200 rounded p-1 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2"
+        />
+        <label>2</label>
+        <input
+          id="priority-3"
+          name="priority"
+          type="radio"
+          onChange={handleChange}
+          required
+          value={3}
+          checked={formData.priority == "3"}
+          className="bg-white text-black border border-gray-200 rounded p-1 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2"
+        />
+        <label>3</label>
+        <input
+          id="priority-4"
+          name="priority"
+          type="radio"
+          onChange={handleChange}
+          required
+          value={4}
+          checked={formData.priority == "4"}
+          className="bg-white text-black border border-gray-200 rounded p-1 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2"
+        />
+        <label>4</label>
+        <input
+          id="priority-5"
+          name="priority"
+          type="radio"
+          onChange={handleChange}
+          required
+          value={5}
+          checked={formData.priority == "5"}
+          className="bg-white text-black border border-gray-200 rounded p-1 focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2"
+        />
+        <label>5</label>
       </form>
     </div>
   );
