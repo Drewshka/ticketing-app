955d9844c15067eb983c43fd97283103a0d149dd
Andrew Stevenson <37091999+Drewshka@users.noreply.github.com>
2025-12-22 17:11:35 -0500

    adding delete functionality

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,12 +1,27 @@
+"use client";
+
 const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome");
 import { fa0, faX } from "@fortawesome/free-solid-svg-icons";
+import { routerServerGlobal } from "next/dist/server/lib/router-utils/router-server-context";
+import { useRouter } from "next/navigation";
 
-const DeleteBlock = () => {
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      router.refresh();
+    }
+  };
   return (
     <div className="flex items-center gap-2">
       <FontAwesomeIcon
         icon={faX}
         className="text-red-400 hover:cursor-pointer hover:text-red-200"
+        onClick={deleteTicket}
       />
     </div>
   );
