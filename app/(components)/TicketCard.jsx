90a8a944dfd0b88e7936c7395271f5a7b36b2b38
Andrew Stevenson <37091999+Drewshka@users.noreply.github.com>
2025-12-22 13:58:11 -0500

    fetching ticket info from Mondo DB

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,26 +3,26 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
-const TicketCard = () => {
+const TicketCard = ({ ticket }) => {
   return (
     <div className="flex flex-col bg-card hovver:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
-        <PriorityDisplay />
+        <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
           <DeleteBlock />
         </div>
       </div>
-      <h4>Ticket Title</h4>
+      <h4>{ticket.title}</h4>
       <hr className="whitespace-pre-wrap" />
-      <p>this is the ticket description! Please do this ticket</p>
+      <p>{ticket.description}</p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">12/20/25 5:30pm</p>
-          <ProgressDisplay />
+          <p className="text-xs my-1">{ticket.createdAt}</p>
+          <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">
-          <StatusDisplay />
+          <StatusDisplay status={ticket.status} />
         </div>
       </div>
     </div>
